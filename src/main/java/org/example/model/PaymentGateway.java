e798c8061010d27c5ae716fe39d472f2efa5c0ae
Sumukh Bhandarkar <sumukhbhandarkar@Sumukhs-MacBook-Pro.local>
2024-10-12 00:51:10 +0530

    Code refactor, added payment gateways

--- src/main/java/org/example/model/PaymentGateway.java
+++ src/main/java/org/example/model/PaymentGateway.java
@@ -3,6 +3,16 @@ package org.example.model;
 public enum PaymentGateway {
     RAZORPAY,
     PAYTM,
-    SBI,
-    CC_AVENUE
+    PAYPAL,
+    CC_AVENUE,
+    UPI_QR,
+    UPI_GPAY,
+    UPI_PHONEPE,
+    UPI_VPA,
+    AMAZON_PAY,
+    PAYU,
+    APPLE_PAY,
+    PAY_U_BIZ,
+    CASHFREE_PAYMENTS,
+    STRIPE
 }
